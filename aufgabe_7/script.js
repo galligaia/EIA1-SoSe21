2ab6c2fa2b43f515cea61d402bb69a6ba8754ecc
galligaia <gaiagalli67@gmail.com>
2021-05-07 16:47:18 +0200

    aufgabe 7

---
+++ aufgabe_7/script.js
@@ -0,0 +1,26 @@
+function playSample(sounds) {
+    var sound = new Audio(sounds);
+    sound.play();
+}
+var beats = ["assets/kick.mp3", "assets/snare.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/hihat.mp3"];
+var index = 0;
+document.querySelector('#drum1').addEventListener('click', function () { playSample("assets/kick.mp3"); });
+document.querySelector('#drum2').addEventListener('click', function () { playSample("assets/snare.mp3"); });
+document.querySelector('#drum3').addEventListener('click', function () { playSample("assets/hihat.mp3"); });
+document.querySelector('#drum4').addEventListener('click', function () { playSample("assets/F.mp3"); });
+document.querySelector('#drum5').addEventListener('click', function () { playSample("assets/G.mp3"); });
+document.querySelector('#drum6').addEventListener('click', function () { playSample("assets/A.mp3"); });
+document.querySelector('#drum7').addEventListener('click', function () { playSample("assets/C.mp3"); });
+document.querySelector('#drum8').addEventListener('click', function () { playSample("assets/laugh-2.mp3"); });
+document.querySelector('#drum9').addEventListener('click', function () { playSample("assets/laugh-1.mp3"); });
+document.querySelector('#playbutton').addEventListener('click', function () {
+    var interval = setInterval(function () {
+        var beat = new Audio(beats[index]);
+        beat.play();
+        index = index + 1;
+        if (index == 5) {
+            index = 0;
+        }
+    }, 300);
+});
+//# sourceMappingURL=script.js.map
